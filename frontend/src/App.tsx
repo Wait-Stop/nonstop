b16df0355e176038bd8fc3d5f94a3e23b921e2ae
jeongyun914 <j.yun@soomkyung.ac.kr>
2026-07-24 20:18:44 +0900

    feat: 지역 상세 및 지역 비교 화면 구현

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,9 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout";
 import { LoginPage, SignupPage } from "./pages/AuthPages";
+import { RegionComparePage, RegionsPage } from "./pages/ExplorePages";
 import HomePage from "./pages/HomePage";
-import { RecommendationsPage } from "./pages/RecommendationPages";
+import { RecommendationsPage, RegionDetailPage } from "./pages/RecommendationPages";
 
 export default function App() {
   return <Routes>
@@ -11,6 +12,9 @@ export default function App() {
       <Route path="/login" element={<LoginPage />} />
       <Route path="/signup" element={<SignupPage />} />
       <Route path="/recommendations" element={<RecommendationsPage />} />
+      <Route path="/regions" element={<RegionsPage />} />
+      <Route path="/regions/compare" element={<RegionComparePage />} />
+      <Route path="/regions/:id" element={<RegionDetailPage />} />
       <Route path="*" element={<Navigate to="/" replace />} />
     </Route>
   </Routes>;
